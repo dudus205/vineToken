dd60df97ae3c10b3b5f4e509aac833df4645c122
Piotr Kaczmarczyk <kaczmarczyk9042@outlook.com>
2023-04-16 21:56:24 +0200

    Added test to smart contract

---
+++ contracts/vineToken.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract VineToken is ERC721, Ownable, ERC721Burnable {
+    uint256 public mintPrice = 0.05 ether; //50000000000000000 Wei = 0.05
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    bool public isMintEnabled;
+    mapping(address => uint256) public mintedWallets;
+
+    constructor() ERC721("vineToken", "VIN") {
+        maxSupply = 2;
+    }
+
+    function toggleIsMintEnabled() external onlyOwner{
+        isMintEnabled = !isMintEnabled;
+    }
+
+    function setMaxSupply(uint256 maxSupply_) external onlyOwner{
+        maxSupply = maxSupply_;
+    }
+
+    function mint() external payable {
+        require(isMintEnabled, 'minting not enabled');
+        require(mintedWallets[msg.sender] < 1, 'exceeds max per wallet');
+        require(msg.value == mintPrice, 'wrong value');
+        require(maxSupply > totalSupply, 'sold out');
+
+        mintedWallets[msg.sender]++;
+        totalSupply++;
+        uint256 tokenID = totalSupply;
+        _safeMint(msg.sender, tokenID);
+    }
+
+}
---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  mocha: {
+    // Load the bundled test file
+    // Note: the path is relative to the root of your project
+    files: ['dist/test.bundle.js'],
+  },
+};
